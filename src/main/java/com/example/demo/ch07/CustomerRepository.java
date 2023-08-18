6442535bce101a211b8b7315ef1b0407f254170d
Min Hong <shmin7777@naver.com>
2023-08-18 16:09:45 +0900

    jpa repository item reader 추가

---
+++ src/main/java/com/example/demo/ch07/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.example.demo.ch07;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Page<Customer> findByCity(String city, Pageable pageable);
+}
